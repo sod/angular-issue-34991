eb7413d19c8e4067328774815ef3bf36c14ed9bb
Alexander von Weiss <alexvonweiss@googlemail.com>
2020-01-27 14:27:23 +0100

    create reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8-ngmodel-autocomplete';
+  value: string;
+  @ViewChild('inputElement', {static: true}) inputElement: ElementRef<HTMLInputElement>;
 }
